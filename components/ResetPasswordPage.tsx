d58ec91bebaccbd81b65226ca4abc8b51a8c7dcd
jackdafox <83963164+jackdafox@users.noreply.github.com>
2025-01-08 18:54:38 +0800

    Refactor ResetPasswordPage and VerifyAccountPage for improved responsiveness and styling; adjust layout and text sizes for better user experience

--- components/ResetPasswordPage.tsx
+++ components/ResetPasswordPage.tsx
@@ -6,17 +6,17 @@ import { User } from "@prisma/client";
 
 const ResetPasswordPage = ({ user }: { user: User }) => {
   return (
-    <div className="flex justify-center items-center p-10">
-      <div className="flex flex-col justify-center items-center text-white w-96 gap-3 max-w-full">
-        <ShopeeSvg />
-        <h1 className="font-bold text-4xl tracking-tight mt-6">
-          Create new password
-        </h1>
-        <p className="text-start px-5">
-          Please enter your new password below for your Spotify account.
-        </p>
+    <div className="flex justify-center items-center p-4 sm:p-10">
+      <div className="flex flex-col justify-center items-center text-white w-full sm:w-96 gap-3 px-4 sm:px-0">
+      <ShopeeSvg />
+      <h1 className="font-bold text-2xl sm:text-4xl tracking-tight mt-6 text-center">
+        Create new password
+      </h1>
+      <p className="text-center sm:text-start px-2 sm:px-5 text-sm sm:text-base">
+        Please enter your new password below for your Spotify account.
+      </p>
 
-        <ResetPasswordForm userID={user.id} />
+      <ResetPasswordForm userID={user.id} />
       </div>
     </div>
   );
--- components/VerifyAccountPage.tsx
+++ components/VerifyAccountPage.tsx
@@ -4,17 +4,17 @@ import LoginForm from './LoginForm'
 
 const VerifyAccountPage = () => {
   return (
-    <div className="flex justify-center items-center p-10">
-      <div className="flex flex-col justify-center items-center text-white w-96 gap-3 max-w-full">
-        <ShopeeSvg />
-        <h1 className="font-bold text-4xl tracking-tight mt-6">
-          Verify your account
-        </h1>
-        <p className="text-start px-5">
-          Please enter your email and password below to verify your account.
-        </p>
+    <div className="flex justify-center items-center p-4 sm:p-6 md:p-10">
+      <div className="flex flex-col justify-center items-center text-white w-full sm:w-[400px] gap-3 px-4 sm:px-0">
+      <ShopeeSvg />
+      <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl tracking-tight mt-4 sm:mt-6 text-center">
+        Verify your account
+      </h1>
+      <p className="text-start px-2 sm:px-5 text-sm sm:text-base">
+        Please enter your email and password below to verify your account.
+      </p>
 
-        <LoginForm/>
+      <LoginForm/>
       </div>
     </div>
   )
